b7bf9c23d4d0a1f634f379865e0f7ad5908fdeb3
lucioric2000 <lucioric2000@hotmail.com>
2021-11-21 02:12:01 -0600

    Tries to fix a Windows installation issue

--- delphifmx/_utils.py
+++ delphifmx/_utils.py
@@ -4,8 +4,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def new_import(internal_name="DelphiFMX", module_full_path=None,
-               pyd_file_basename="DelphiFMX.pyd", external_name="delphifmx"):
+def new_import(internal_name=None, module_full_path=None,
+               pyd_file_basename=None, external_name="delphifmx"):
+    platmac = platform.machine()
+    platsys = platform.system()
+    if platsys == "Windows":
+        pyd_file_basename = "DelphiFMX.pyd"
+    elif platsys == "Linux":
+        pyd_file_basename = "libDelphiFMX.so"
+    elif platsys == "Darwin":
+        pyd_file_basename = "libDelphiFMX.dylib"
+
     if module_full_path is None:
         dirbname_full = os.path.dirname(os.path.abspath(__file__))
         module_full_path = os.path.join(dirbname_full, pyd_file_basename)
@@ -19,24 +28,3 @@ def new_import(internal_name="DelphiFMX", module_full_path=None,
     sys.modules[external_name] = package
     spec.loader.exec_module(package)
     return package
-
-
-def find_module(internal_name="DelphiFMX"):
-    platmac = platform.machine()
-    if platmac.endswith('64'):
-      # Win x64
-      platmacshort = "Win64"
-    else:
-      # Win x86
-      platmacshort = "Win32"
-
-    dir_last_level = f"{internal_name}_{platmacshort}_{sys.version_info.major}{sys.version_info.minor}"
-    my_folder = os.path.dirname(os.path.abspath(os.path.join(os.getcwd(), __file__)))
-    dirbname_full = os.path.normpath(os.path.join(my_folder, "..", "lib", dir_last_level))
-    internal_name_lower = internal_name.lower()
-
-    for fname in os.listdir(dirbname_full):
-        #Do a case-insensitive search for the .pyd file
-        if internal_name_lower in fname.lower():
-            return os.path.join(dirbname_full, fname)
-    return None
